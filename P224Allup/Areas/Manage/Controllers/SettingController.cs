cd4cbd7ba69ad111b7d6d9866e2a4d28e9f7ca18
HamidMammadov <hamidvm@code.edu.az>
2022-04-18 18:39:08 +0400

    First Commit

---
+++ P224Allup/Areas/Manage/Controllers/SettingController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P224Allup.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P224Allup.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class SettingController : Controller
+    {
+        private readonly AllupDbContext _context;
+
+        public SettingController(AllupDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Settings.FirstOrDefaultAsync());
+        }
+
+        public async Task<IActionResult> Detail()
+        {
+            return View(await _context.Settings.FirstOrDefaultAsync());
+        }
+
+        public async Task<IActionResult> Update()
+        {
+            return View(await _context.Settings.FirstOrDefaultAsync());
+        }
+    }
+}
---
+++ P224Allup/Models/Setting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P224Allup.Models
+{
+    public class Setting : BaseEntity
+    {
+        [StringLength(255),Required]
+        public string Logo { get; set; }
+        [StringLength(255),Required]
+        public string Offer { get; set; }
+        [StringLength(255),Required,EmailAddress]
+        public string Email { get; set; }
+        [StringLength(255),Required]
+        public string SupportPhone { get; set; }
+        [StringLength(255),Required]
+        public string Phone { get; set; }
+        [StringLength(255),Required]
+        public string Address { get; set; }
+        [StringLength(255),Required]
+        public string WorkDay { get; set; }
+    }
+}
